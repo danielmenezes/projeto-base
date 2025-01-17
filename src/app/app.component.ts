f267ca5f821cc0a13bad281a15edc796ebb68eb9
Daniel Menezes <daniel.menezes.md@gmail.com>
2025-01-17 13:33:36 -0300

    criado componente de input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputComponent } from './design-system/components/input/input.component';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, FormsModule, MatFormFieldModule, InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'projeto-base';
+  form: FormGroup;
+  max = 10
+  showMessageError = false
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      userName: [{ value: '', disabled: false }, [Validators.required]], 
+    });
+  }
+
+  mostrarErros() {
+    this.showMessageError = true;
+    console.log(this.form.controls['userName'].value)
+  }
+
+
 }
---
+++ src/app/design-system/components/input/input.component.ts
@@ -0,0 +1,141 @@
+import { Component, Input, OnChanges, OnInit, Optional, Self, SimpleChanges } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ControlValueAccessor, FormControl, FormsModule, NgControl, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+
+type MatInputType =
+  | 'color'
+  | 'number'
+  | 'password'
+  | 'search'
+  | 'text'
+  | 'time'
+  | 'coin';
+
+@Component({
+  selector: 'app-input',
+  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule, FormsModule, NgxMaskDirective],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss',
+  providers: [provideNgxMask()]
+})
+export class InputComponent implements OnInit, OnChanges, ControlValueAccessor {
+
+  @Input() type: MatInputType = 'text';
+  @Input() label: string = '';
+  @Input() placeholder: string = '';
+  @Input() required: boolean = false;
+  @Input() showMessageError: boolean = false;
+  @Input() messageError: string = '';
+  @Input() characterCounter: boolean = false;
+  @Input() maxLength: number = Infinity;
+  @Input() allowNegativeNumbers: boolean = false;
+  @Input() max: number = Infinity;
+  @Input() min: number = 0;
+
+
+  private onChange: any = (value: any) => {};
+  private onTouched: any = () => {};
+  control: FormControl = new FormControl();
+  previousValue = '';
+
+  constructor(@Optional() @Self() public ngControl: NgControl) {
+    if(this.ngControl) {
+      this.ngControl.valueAccessor = this;
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.showMessageError && changes['showMessageError']) {
+      this.control.markAsTouched();
+    }
+  }
+
+  ngOnInit(): void {
+    const validators = this.ngControl?.control?.validator;
+    this.control.setValidators(validators ? validators : null);
+    this.control.updateValueAndValidity();
+  }
+
+  writeValue(value: any): void {
+    this.control.patchValue(value || '');
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  onInputChange(): void {
+    let value = this.control.value;
+    
+    if((this.type === 'number' || this.type === 'coin') && value) {
+      
+      if(this.type === 'coin') {
+        value = value.replace(/-R\$ /g, '-').replace(/R\$ /g, '').replace('.', '').replace(',', '.');
+        
+        if (value?.replace(/.$/g, '') == this.max) {
+          value = value?.replace(/.$/g, '');
+          this.control.patchValue(value);
+        }
+      }
+
+      const numericValue = parseFloat(value);
+
+      if (numericValue > this.max) {
+        if(!Number.isInteger(this.max)) { // Para verificar se o max é um valor em ponto flutuante.
+          const lastDigit = value.slice(-1);
+          value = `${value.slice(0,-1)}.${lastDigit}`;
+          this.control.patchValue(value);
+          this.previousValue = value;
+          this.onChange(value);
+          return;
+        }
+
+        this.control.patchValue(this.previousValue);
+        return;
+      }
+
+      if (numericValue < this.min) {
+        if(!Number.isInteger(this.min)) { // Para verificar se o max é um valor em ponto flutuante.
+          const lastDigit = value.slice(-1);
+          value = `${value.slice(0,-1)}.${lastDigit}`;
+          this.control.patchValue(value);
+          this.previousValue = value;
+          this.onChange(value);
+          return;
+        }
+
+        this.control.patchValue(this.previousValue);
+        return;
+      }
+
+      this.previousValue = value;
+    }
+    console.log('aqui')
+    this.onChange(value);
+  }
+
+  onInputTouche() {
+    this.onTouched()
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    if (isDisabled) {
+      this.control.disable();
+    } else {
+      this.control.enable();
+    }
+  }
+
+}
+
+
+
+  
+
+
